0f8283d266f299f8cfc0328337f03290f3aab2cc
lucas rego <lucasalvesrego.contato@gmail.com>
2024-04-30 18:59:19 -0300

    feat: custom client

--- apps/vannila/src/websocket-client.ts
+++ apps/vannila/src/websocket-client.ts
@@ -1,10 +1,1 @@
-export const wsClient = new WebSocket('ws://localhost:8080');
-
-wsClient.onmessage = (ev) => {
-  console.log('Mensagem recebida:', ev.data);
-  wsClient.send('Olá, servidor!');
-};
-
-wsClient.onclose = (ev) => {
-  wsClient.send('close connection' + ev.code + ev.reason);
-};
+export const ws = new WebSocket('ws://localhost:8000/ws');
